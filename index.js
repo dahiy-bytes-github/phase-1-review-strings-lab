f5fd6615bf1af8f438b4ca14cd7b8aca41a0af29
Abdullahi <aadahiye@gmail.com>
2024-09-21 07:32:39 +0300

    Completes strings lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'AAD4'
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + '!'
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser +'!'
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
